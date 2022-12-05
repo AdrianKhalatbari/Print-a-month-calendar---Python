44472aeb74b8c0112b8a66047eeabf6ddd7dd9f2
Mohammad Khalatbari <khalatbari.computer@gmail.com>
2022-12-05 17:45:01 +0200

    Update Print a month calendar.py

--- Print a month calendar.py
+++ Print a month calendar.py
@@ -1,12 +1,9 @@
 import calendar
 
-
 print('This program prints the calendar of a desired month.')
-
 yy = int(input('Give me the year:\n'))
 mm = int(input('Give the month:\n'))
 print('_____________________')
-
 # display the calendar
 dateStr = calendar.month(yy, mm)
 print(dateStr)
